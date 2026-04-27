47e688f747a59156136c58e99a28647344651d84
vivekgudikandula-source <vivekgudikandula@gmail.com>
2026-04-27 23:32:27 +0530

    msg2

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
+python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py collectstatic --noinput
-gunicorn jango_deployment.wsgi:application --bind 0.0.0.0 :$PORT
+gunicorn jango_deployment.wsgi:application --bind 0.0.0.0:$PORT
