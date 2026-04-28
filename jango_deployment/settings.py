c18020d420e782df95ab0ea34c7a04e8026e201f
vivekgudikandula-source <vivekgudikandula@gmail.com>
2026-04-28 13:16:37 +0530

    msg4

--- jango_deployment/settings.py
+++ jango_deployment/settings.py
@@ -73,46 +73,46 @@ WSGI_APPLICATION = 'jango_deployment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-# import os
-# if os.environ.get("RENDER"):
-#     # production (render + aliven)
-#     DATABASES={
-#         "default":{
-#             "ENGINE":"django.db.backends.mysql",
-           
-#             "NAME":os.environ.get("MYSQLDATABASE"),
-#             "USER":os.environ.get("MYSQLUSER"),
-#             "PASSWORD":os.environ.get("MYSQLPASSWORD"),
-#             "HOST":os.environ.get("MYSQLHOST"),
-#             "PORT":os.environ.get("MYSQLPORT","3306"),
-#         }
-#     }
-# else:
-#       # LOCAL DEVELOPMENT
-#       DATABASES={
-#         "default":{
-#             "ENGINE":"django.db.backends.mysql",
-#             "NAME":"jango_deploy",
-#             "USER":"root",
-#             "PASSWORD":"viveksql",
-#             "HOST":"localhost",
-#             "PORT":"3306",
-#         }
-#     }
-
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+import os
+if os.environ.get("RENDER"):
+    # production (render + aliven)
+    DATABASES={
+        "default":{
+            "ENGINE":"django.db.backends.mysql",
+           
+            "NAME":os.environ.get("MYSQLDATABASE"),
+            "USER":os.environ.get("MYSQLUSER"),
+            "PASSWORD":os.environ.get("MYSQLPASSWORD"),
+            "HOST":os.environ.get("MYSQLHOST"),
+            "PORT":os.environ.get("MYSQLPORT","3306"),
+        }
+    }
+else:
+      # LOCAL DEVELOPMENT
+      DATABASES={
+        "default":{
+            "ENGINE":"django.db.backends.mysql",
+            "NAME":"jango_deploy",
+            "USER":"root",
+            "PASSWORD":"viveksql",
+            "HOST":"localhost",
+            "PORT":"3306",
+        }
+    }
+
+# import dj_database_url
+
+# DATABASES = {
+#     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+# }
 
 
 # Password validation
